e48060f45ab30da6ed7245884b1527404313fb1a
vinay-3333 <124817464+vinay-3333@users.noreply.github.com>
2023-12-14 10:19:53 +0000

    componentand pipline creation

--- src/CNNClassifier/entity/config_entity.py
+++ src/CNNClassifier/entity/config_entity.py
@@ -9,3 +9,38 @@ class DataIngestionConfig: #forzen use for to avoid accidental modification and 
     Source_URL:str
     local_data_file:Path
     unzip_dir:Path
+
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top:bool
+    params_weights: str
+    params_classes: int
+
+
+
+    
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    params_image_size: list
+    params_batch_size: int
+    
